009e6c117267a0af25bbfef92cafcb4c57a034bd
Abhijit Suprem <asuprem@gatech.edu>
2020-09-25 10:33:43 -0400

    ✨feature: Added serializers

---
+++ python/ebka/src/ebka/serializers/EmptySerializer.py
@@ -0,0 +1,31 @@
+from ebka.serializers import Serializable
+
+class EmptyStringSerializer(Serializable):
+    def read(self, in_stream: str):
+        return in_stream
+    def write(self, out_stream: str):
+        return out_stream
+
+class EmptyBoolSerializer(Serializable):
+    def read(self, in_stream: bool):
+        return in_stream
+    def write(self, out_stream: bool):
+        return out_stream
+
+class EmptyIntSerializer(Serializable):
+    def read(self, in_stream: int):
+        return in_stream
+    def write(self, out_stream: int):
+        return out_stream
+
+class EmptyFloatSerializer(Serializable):
+    def read(self, in_stream: float):
+        return in_stream
+    def write(self, out_stream: float):
+        return out_stream
+
+class EmptyByteSerializer(Serializable):
+    def read(self, in_stream: bytes):
+        return in_stream
+    def write(self, out_stream: bytes):
+        return out_stream
