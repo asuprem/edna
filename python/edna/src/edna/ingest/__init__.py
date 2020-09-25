0000c4d150236b63e0db38c956e034c32e1ecf1f
Abhijit Suprem <asuprem@gatech.edu>
2020-09-25 18:34:05 -0400

    🐞fix: Added IngestPattern enum to BaseIngest class

--- python/edna/src/edna/ingest/__init__.py
+++ python/edna/src/edna/ingest/__init__.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
 from edna.serializers import Serializable
-
+from edna.core.types.enums import IngestPattern
 class BaseIngest(object):
+    execution_mode: IngestPattern
     def __init__(self, serializer: Serializable):
         self.serializer = serializer
 
