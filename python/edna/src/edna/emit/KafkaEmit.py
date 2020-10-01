c234967725d6382563dc3ad0ab365c3a6ebecd63
Abhijit Suprem <asuprem@gatech.edu>
2020-10-01 15:33:32 -0400

    🐞fix: Added create_topic to KafkaEmit

--- python/edna/src/edna/emit/KafkaEmit.py
+++ python/edna/src/edna/emit/KafkaEmit.py
@@ -1,21 +1,34 @@
+from typing import Dict
 from edna.emit import BaseEmit
 from edna.serializers import Serializable
 
-from confluent_kafka import Producer
+from typing import Dict
+from time import sleep
+from confluent_kafka import Producer, admin
 import socket
 class KafkaEmit(BaseEmit):
     """An Emitter that writes to a Kafka topic"""
-    def __init__(self, serializer: Serializable, kafka_topic: str, bootstrap_server: str = None):   # For java, need to ensure it is a bytesSerializer
+    def __init__(self, serializer: Serializable, kafka_topic: str, bootstrap_server: str = "localhost", bootstrap_port: int = 9092):   # For java, need to ensure it is a bytesSerializer
 
         self.kafka_topic = kafka_topic
         conf = {
-            "bootstrap.servers": bootstrap_server if bootstrap_server is not None else "localhost",
+            "bootstrap.servers": bootstrap_server + ":" + str(bootstrap_port),
             "client.id":socket.gethostname()
         }
+
+        self.create_topic(topic_name=kafka_topic, conf=conf)
+        
         self.producer = Producer(conf)
         super().__init__(serializer=serializer)
 
     def write(self, message: bytes):
-        self.producer.produce(self.kafka_topic, value = message)
+        self.producer.produce(self.kafka_topic, value = message)    # Already serialized.
 
-        
+    def create_topic(self, topic_name: str, conf: Dict):
+        adminclient = admin.AdminClient(conf=conf)
+        topic = admin.NewTopic(topic=topic_name, num_partitions=1)
+        response = adminclient.create_topics([topic])
+        while not response[topic_name].done():
+            sleep(0.001)    # TODO this is super hacky. There is bound to be a better way to do this.
+        del adminclient
+            
