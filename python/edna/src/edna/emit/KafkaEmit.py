ffc590473dd43781a64f7ccc97639866413a09eb
Abhijit Suprem <asuprem@gatech.edu>
2020-10-02 01:20:43 -0400

    📄docs: Added docs for KafkaEmit

--- python/edna/src/edna/emit/KafkaEmit.py
+++ python/edna/src/edna/emit/KafkaEmit.py
@@ -4,11 +4,19 @@ from edna.serializers import Serializable
 
 from typing import Dict
 from time import sleep
-from confluent_kafka import Producer, admin
+import confluent_kafka
 import socket
 class KafkaEmit(BaseEmit):
-    """An Emitter that writes to a Kafka topic"""
+    """An Emitter that writes to a Kafka topic."""
     def __init__(self, serializer: Serializable, kafka_topic: str, bootstrap_server: str = "localhost", bootstrap_port: int = 9092):   # For java, need to ensure it is a bytesSerializer
+        """Connects to a specified kafka topic and sets up the emitter.
+
+        Args:
+            serializer (Serializable): Serializer to convert a message to bytes before sending to kafka.
+            kafka_topic (str): Name of kafka topic to publish to.
+            bootstrap_server (str, optional): Address of the Kafka bootstrap server. Defaults to "localhost".
+            bootstrap_port (int, optional): Bootstrap server port on which the topic is listening for messages. Defaults to 9092.
+        """
 
         self.kafka_topic = kafka_topic
         conf = {
@@ -18,17 +26,29 @@ class KafkaEmit(BaseEmit):
 
         self.create_topic(topic_name=kafka_topic, conf=conf)
         
-        self.producer = Producer(conf)
+        self.producer = confluent_kafka.Producer(conf)
         super().__init__(serializer=serializer)
 
     def write(self, message: bytes):
+        """Publishes a message to the instance's saved kafka topic.
+
+        Args:
+            message (bytes): Serialized byte-encoded message to publish.
+        """
         self.producer.produce(self.kafka_topic, value = message)    # Already serialized.
 
     def create_topic(self, topic_name: str, conf: Dict):
-        adminclient = admin.AdminClient(conf=conf)
-        topic = admin.NewTopic(topic=topic_name, num_partitions=1)
+        """Creates a kafka topic using the admin-client api from confluent.
+
+        Args:
+            topic_name (str): Name of the topic to create
+            conf (Dict): The kafka configuration, containing bootstrap 
+            server address and client id
+        """
+        adminclient = confluent_kafka.admin.AdminClient(conf=conf)
+        topic = confluent_kafka.admin.NewTopic(topic=topic_name, num_partitions=1)
         response = adminclient.create_topics([topic])
         while not response[topic_name].done():
             sleep(0.001)    # TODO this is super hacky. There is bound to be a better way to do this.
         del adminclient
-            
+
