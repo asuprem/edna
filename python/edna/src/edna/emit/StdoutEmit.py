73ebde6e60653b5469ae65b2545675900b0a60fa
Abhijit Suprem <asuprem@gatech.edu>
2020-10-02 01:20:51 -0400

    📄docs: Added docs for StdOut

--- python/edna/src/edna/emit/StdoutEmit.py
+++ python/edna/src/edna/emit/StdoutEmit.py
@@ -3,8 +3,13 @@ from edna.emit import BaseEmit
 from sys import stdout
 
 class StdoutEmit(BaseEmit):
-    """An Emitter that writes to stdout"""
-    def write(self, message):
+    """An Emitter that writes to Stdout"""
+    def write(self, message: str):
+        """Writes the message to the standard output. Ideally, the message should be a string. THis is useful only for debugging.
+
+        Args:
+            message (str): The message to write
+        """
         print(message, file=stdout)
 
         
