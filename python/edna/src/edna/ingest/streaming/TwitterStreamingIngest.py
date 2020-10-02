57b7a119702c068aae298e1c47f8fc29d6c388af
Abhijit Suprem <asuprem@gatech.edu>
2020-10-02 01:21:39 -0400

    📄docs:  Added docs for TwitterStreamingIngest

--- python/edna/src/edna/ingest/streaming/TwitterStreamingIngest.py
+++ python/edna/src/edna/ingest/streaming/TwitterStreamingIngest.py
@@ -1,4 +1,9 @@
 from edna.ingest.streaming.BaseTwitterIngest import BaseTwitterIngest
 
 class TwitterStreamingIngest(BaseTwitterIngest):
+    """Class for streaming from Twitter using the v2 API endpoints.
+
+    Attributes:
+        base_url (str): The endpoint for the streaming or filter request.
+    """
     base_url = "https://api.twitter.com/2/tweets/sample/stream?"
