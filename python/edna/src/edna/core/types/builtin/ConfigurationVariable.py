5048bd25be1e6f0ee9c40c6b0f38c76782331740
Abhijit Suprem <asuprem@gatech.edu>
2020-10-02 01:20:03 -0400

    📄docs: Added docs for ConfigurationVariable

--- python/edna/src/edna/core/types/builtin/ConfigurationVariable.py
+++ python/edna/src/edna/core/types/builtin/ConfigurationVariable.py
@@ -1,5 +1,15 @@
 
 
 class ConfigurationVariable(dict):
-    def __getitem__(self, k):
+    """Wrapper around dict to store a single Configuration type.
+    """
+    def __getitem__(self, k: str):
+        """Get a value from a key-value pair in the Configuration. Wrapper around dict.__getitem__()
+
+        Args:
+            k (str): Key for the desired key-value pair.
+
+        Returns:
+            (str): The value of the key-value pair.
+        """
         return super().__getitem__(k)   # TODO return edna error?
