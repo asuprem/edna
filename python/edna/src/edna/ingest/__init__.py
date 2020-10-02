e6ba878e269af04c4e2c83f6284438a869a7b7d2
Abhijit Suprem <asuprem@gatech.edu>
2020-10-02 01:21:02 -0400

    📄docs:  Added docs for BaseIngest

--- python/edna/src/edna/ingest/__init__.py
+++ python/edna/src/edna/ingest/__init__.py
@@ -2,6 +2,18 @@ from __future__ import annotations
 from edna.serializers import Serializable
 from edna.core.types.enums import IngestPattern
 class BaseIngest(object):
+    """BaseIngest is the base interface for ingesting messages from various sources, 
+    either external or internal (i.e. Kafka).
+
+    Any child class must:
+
+    - Call `super().__init__()` at the end of initialization
+    
+    - Create an `execution_mode` in `edna.core.types.enums.IngestPattern`
+    
+    - Create an execution pattern in the `run()` method of the correct 
+        `edna.core.execution.context.EdnaContext`.
+    """
     execution_mode: IngestPattern
     def __init__(self, serializer: Serializable):
         self.serializer = serializer
