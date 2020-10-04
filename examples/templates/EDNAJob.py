903efb82b2c1b6161cf55af6d6982c14d5d8d9ff
Abhijit Suprem <asuprem@gatech.edu>
2020-10-03 22:53:22 -0400

    📄docs: Added templates for job python file, config.yaml, and ednaconf.yaml

---
+++ examples/templates/EDNAJob.py
@@ -0,0 +1,28 @@
+from edna.core.execution.context import EdnaContext
+from edna.ingest import BaseIngest
+from edna.process import BaseProcess
+from edna.emit import BaseEmit
+from edna.serializers import Serializable
+
+# This file will not work, because it ues only the Base Classes, which was empty.
+# You can sue this as a starting point -- replace all the base classes with your needed classes
+
+def main():
+    context = EdnaContext()     # Choose an appropriate context, such as StreamingContext
+    
+    ingest_serializer = Serializable()  # e.g. KafkaStringSerializer
+    emit_serializer = Serializable()    # e.g. StringSerializer
+    
+    ingest = BaseIngest(ingest_serializer, context.getVariable("VARIABLE1"))    # e.g. KafkaIngest
+    process = BaseProcess(context.getVariable("VARIABLE2"))                     # e.g. BaseProcess
+    emit = BaseEmit(emit_serializer, context.getVariable("VARIABLE1"))          # e.g. KafkaEmit
+
+    context.addIngest(ingest=ingest)        # Registers the ingest primitive
+    context.addProcess(process=process)     # Registers the process primitive
+    context.addEmit(emit=emit)              # Registers the emit primitive
+
+    context.execute()   # Executes  the context
+
+
+if __name__ == "__main__":
+    main()
