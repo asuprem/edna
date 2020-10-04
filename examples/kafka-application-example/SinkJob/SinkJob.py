b0438096b44ca68d3581faa37b2f7daccc95d837
Abhijit Suprem <asuprem@gatech.edu>
2020-10-03 22:55:28 -0400

    📄docs: Created Kafka-Application-Example

---
+++ examples/kafka-application-example/SinkJob/SinkJob.py
@@ -0,0 +1,33 @@
+from edna.core.execution.context import StreamingContext
+from edna.ingest.streaming import KafkaIngest
+from edna.process import BaseProcess
+from edna.emit import KafkaEmit
+from edna.serializers import StringSerializer
+from edna.serializers import KafkaStringSerializer
+
+# This file will not work, because it ues only the Base Classes, which was empty.
+# You can sue this as a starting point -- replace all the base classes with your needed classes
+
+def main():
+    context = StreamingContext()     # Choose an appropriate context, such as StreamingContext
+    
+    ingest_serializer = StringSerializer()  # e.g. KafkaStringSerializer
+    emit_serializer = KafkaStringSerializer()    # e.g. StringSerializer
+    
+    ingest = KafkaIngest(ingest_serializer, 
+        kafka_topic=context.getVariable("import_key"),
+        bootstrap_server=context.getVariable("bootstrap_server"))    # e.g. KafkaIngest
+    process = BaseProcess()                     # e.g. BaseProcess
+    emit = KafkaEmit(emit_serializer, 
+        kafka_topic=context.getVariable("export_key"),
+        bootstrap_server=context.getVariable("bootsrap_server"))           # e.g. KafkaEmit
+
+    context.addIngest(ingest=ingest)        # Registers the ingest primitive
+    context.addProcess(process=process)     # Registers the process primitive
+    context.addEmit(emit=emit)              # Registers the emit primitive
+
+    context.execute()   # Executes  the context
+
+
+if __name__ == "__main__":
+    main()
