eaba8cf39be319d55156d081315b36bf6df97dc1
Abhijit Suprem <asuprem@gatech.edu>
2020-10-06 01:47:48 -0400

    🐞fix: bug fix for empty variables set in conf file

--- python/edna/src/edna/core/configuration/StreamingConfiguration.py
+++ python/edna/src/edna/core/configuration/StreamingConfiguration.py
@@ -6,8 +6,9 @@ class StreamingConfiguration(EdnaConfiguration):
     
     """
     def set_variables(self, configuration: Dict[str, str]):
-        for key in configuration["variables"]:
-            self.setVariable(key, configuration["variables"][key])
+        if configuration["variables"] is not None:
+            for key in configuration["variables"]:
+                self.setVariable(key, configuration["variables"][key])
 
     def set_options(self, configuration: Dict[str, str]):
         pass    # TODO add conf options for streaming configuration here
