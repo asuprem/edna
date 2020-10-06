1f7add564095071cbaecaf766ab261246c61f52c
Abhijit Suprem <asuprem@gatech.edu>
2020-10-06 01:48:25 -0400

    ✨feature: Added Sklearn Classifier Map primitive

---
+++ python/edna/src/edna/process/map/SklearnClassifier.py
@@ -0,0 +1,31 @@
+from os import PathLike
+from edna.process import Map, BaseProcess
+import warnings
+
+
+sklearn = None
+try:
+    import sklearn
+    import joblib
+    import numpy
+except ImportError:
+    warnings.warn("scikit-learn module is not installed. edna.process.map.SklearnClassifier might not work properly.", category=ImportWarning)
+
+
+
+class SklearnClassifier(Map):
+    classifier : sklearn.base.BaseEstimator
+    def __init__(self, process: BaseProcess = None, 
+        classifier_path: PathLike = None, 
+        *args, **kwargs) -> BaseProcess:
+        if classifier_path is None:
+            raise ValueError("Must provide valid `classifier_path` for SklearnClassifier, got None")
+        self.classifier_path = classifier_path
+        self.classifier = joblib.load(classifier_path)
+        super().__init__(process=process, *args, **kwargs)
+
+    def map(self,message : object):
+        return self.classifier.predict(message)
+
+
+        
