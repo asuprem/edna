c4f4b8238f8af230ee2818a9cabc5391273d8edc
Abhijit Suprem <asuprem@gatech.edu>
2020-10-06 01:50:55 -0400

    🐞fix: Fixed incorrect function signature

--- python/edna/src/edna/emit/StdoutEmit.py
+++ python/edna/src/edna/emit/StdoutEmit.py
@@ -4,12 +4,10 @@ from sys import stdout
 
 class StdoutEmit(BaseEmit):
     """An Emitter that writes to Stdout"""
-    def write(self, message: str):
-        """Writes the message to the standard output. Ideally, the message should be a string. THis is useful only for debugging.
-
-        Args:
-            message (str): The message to write
-        """
-        print(message, file=stdout)
+    def write(self):
+        """Writes the message to the standard output. Ideally, the message should be a string. This is useful only for debugging."""
+        # NOT an efficient method, but really, who uses this for a real Job?
+        for buffer_idx in range(self.emit_buffer_index+1):
+            print(self.emit_buffer[buffer_idx], file=stdout)
 
         
