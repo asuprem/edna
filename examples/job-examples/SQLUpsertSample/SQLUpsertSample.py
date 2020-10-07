15b54c86324711a0ab5946d4a368695dab9e9f4b
Abhijit Suprem <asuprem@gatech.edu>
2020-10-07 02:26:36 -0400

    📄docs: Added SQLUpsertSample

---
+++ examples/job-examples/SQLUpsertSample/SQLUpsertSample.py
@@ -0,0 +1,44 @@
+from edna.core.execution.context import StreamingContext
+
+from edna.ingest.streaming import SimulatedIngest
+from edna.process.map import JsonToObject, ObjectToSQL
+from edna.core.factories import SQLTupleFactory
+from edna.emit import SQLUpsertEmit
+
+from edna.serializers.EmptySerializer import EmptyStringSerializer
+from edna.serializers.EmptySerializer import EmptyObjectSerializer
+
+def main():
+
+    list_of_inserts = ['{"actor_id":210, "first_name":"jess", "last_name":"st. german", "additional":"unneeded1"}',
+            '{"actor_id":201, "first_name":"jess", "last_name":"courtney", "additional":"unneeded2"}', 
+            '{"actor_id":202, "first_name":"jess", "last_name":"mishra", "additional":"unneeded3"}', 
+            '{"actor_id":203, "first_name":"jess", "last_name":"novinha", "additional":"unneeded4"}',
+            '{"actor_id":204, "first_name":"jess", "last_name":"changed", "additional":"unneeded5"}',
+            '{"actor_id":205, "first_name":"jess", "last_name":"ael-rayya", "additional":"unneeded6"}', 
+            '{"actor_id":206, "first_name":"jess", "last_name":"zuma", "additional":"unneeded7"}', 
+            '{"actor_id":207, "first_name":"jess", "last_name":"changed", "additional":"unneeded8"}',
+            '{"actor_id":208, "first_name":"jess", "last_name":"changed", "additional":"unneeded9"}',
+            '{"actor_id":209, "first_name":"jess", "last_name":"changed", "additional":"unneeded10"}']
+
+    context = StreamingContext()
+    tuple_factory = SQLTupleFactory(tuple_fields=context.getVariable("sql_fields"), upsert_fields=context.getVariable("upsert_fields")) 
+
+    ingest = SimulatedIngest(serializer=EmptyStringSerializer, stream_list=list_of_inserts)
+    process = ObjectToSQL(process=JsonToObject(), tuple_factory=tuple_factory)
+    emit = SQLUpsertEmit(serializer=EmptyObjectSerializer, 
+        database="sakila", 
+        host="172.28.144.1", 
+        user="asuprem", 
+        password="scientia",
+        table="actor",
+        tuple_factory=tuple_factory)
+
+    context.addIngest(ingest=ingest)
+    context.addProcess(process=process)
+    context.addEmit(emit=emit)
+
+    context.execute()
+
+if __name__ == "__main__":
+    main()
