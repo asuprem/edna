eeaf1f0bcd568ff33453158e2e957fd64a36895f
Abhijit Suprem <asuprem@gatech.edu>
2020-10-20 00:19:03 -0400

    ✨feature: Changed StreamingContext() to SimpleStreamingContext()

---
+++ python/edna/src/edna/core/execution/context/SimpleStreamingContext.py
@@ -0,0 +1,87 @@
+from edna.core.execution.context import EdnaContext
+from edna.core.configuration import StreamingConfiguration
+from edna.types.enums import IngestPattern
+import concurrent.futures
+
+from edna.exception import PrimitiveNotSetException
+
+from edna.ingest import BaseIngest
+from edna.process import BaseProcess
+from edna.emit import BaseEmit
+
+
+class SimpleStreamingContext(EdnaContext):
+    """A `SimpleStreamingContext` is the context for a simple EDNA Job. TODO update this.
+    It uses 2 threads - 1 thread for the ingest, and 1 thread for the process and emit.
+    It provides methods to control the job execution and to configure job variables.
+    See docs for edna.core.execution.context.EdnaContext to initialize
+
+    Args:
+        EdnaContext ([Abstrract Base Class]): An abstract class for all Contexts that provides the interface for interacting with a Context.
+    """        
+    def __init__(self, dir : str = ".", confpath : str = "ednaconf.yaml", confclass: StreamingConfiguration = StreamingConfiguration):
+        """Initialize the SimpleStreamingContext to accept an EDNA job and configuration.
+
+        Args:
+            dir (str, optional): [The directory for the job configuration]. Defaults to the current directory "."
+            confpath (str, optional): [A YAML configuration file for the job. Job variables are loaded as top-level 
+                fields from this file]. Defaults to "ednaconf.yaml".
+            confclass (StreamingConfiguration, optional): [Object to store and interact with the Configuration]. 
+                Defaults to edna.core.configuration.StreamingConfiguration.
+        """        
+        self.ingest_executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+        super().__init__(dir=dir, confpath=confpath, confclass=confclass)
+    
+    def addIngest(self, ingest: BaseIngest):
+        """Adds the provided ingest primitive to the SimpleStreamingContext
+
+        Args:
+            ingest (BaseIngest): Ingest primitive for the SimpleStreamingContext
+        """
+        self.ingest = ingest
+    
+    def addProcess(self, process: BaseProcess):
+        """Adds the provided process primitive to the SimpleStreamingContext
+
+        Args:
+            ingest (BaseProcess): Process primitive for the SimpleStreamingContext
+        """
+        self.process = process
+
+    def addEmit(self, emit: BaseEmit):
+        """Adds the provided emit primitive to the SimpleStreamingContext
+
+        Args:
+            ingest (BaseEmit): Emit primitive for the SimpleStreamingContext
+        """
+        self.emit = emit
+
+    def run(self):  # TODO This should run on a separate thread to handle interrupts
+        """This executes the a job for the SimpleStreamingContext. `run` is called through the `execute()` method in the interface.
+
+        Raises:
+            NotImplementedError: SERVER_SIDE_STREAM patterns are not implemented (and likely won't be, since the pattern is irrelevant)
+        """        
+        if self.ingest is None:
+            raise PrimitiveNotSetException("StreamingContext", "Ingest")
+        if self.process is None:
+            raise PrimitiveNotSetException("StreamingContext", "Process")
+        if self.emit is None:
+            raise PrimitiveNotSetException("StreamingContext", "Emit")
+
+        if self.ingest.execution_mode == IngestPattern.CLIENT_SIDE_STREAM:
+            while True:
+                record_future = self.ingest_executor.submit(next, self.ingest)
+                while not record_future.done():
+                    pass    # Perform buffering?
+                streaming_record = record_future.result()
+                self.emit(self.process(streaming_record)) # Serialization verification TODO
+        if self.ingest.execution_mode == IngestPattern.SERVER_SIDE_STREAM:
+            raise NotImplementedError
+
+    
+            
+
+        
+    
+
--- python/edna/src/edna/core/execution/context/__init__.py
+++ python/edna/src/edna/core/execution/context/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from edna.exception import PrimitiveNotSetException
+
 from edna.core.configuration import EdnaConfiguration
 from edna.ingest import BaseIngest
 from edna.process import BaseProcess
@@ -9,7 +9,7 @@ from abc import ABC
 import os
 
 class EdnaContext(ABC):
-    """An EdnaContext is the generic context for any EdnaJob. It is an interface for the StreamingContext and other future contexts.
+    """An EdnaContext is the generic context for any EdnaJob. It is an interface for the SimpleStreamingContext and other future contexts.
 
     Attributes:
         configuration (EdnaConfiguration): Stores a EdnaConfiguration object to control the job configuration.
@@ -70,29 +70,7 @@ class EdnaContext(ABC):
         """        
         return self.configuration.getVariable(key)
 
-    def addIngest(self, ingest: BaseIngest):
-        """Adds the provided ingest primitive to the EdnaContext
-
-        Args:
-            ingest (BaseIngest): Ingest primitive for the EdnaContext
-        """
-        self.ingest = ingest
     
-    def addProcess(self, process: BaseProcess):
-        """Adds the provided process primitive to the EdnaContext
-
-        Args:
-            ingest (BaseProcess): Process primitive for the EdnaContext
-        """
-        self.process = process
-
-    def addEmit(self, emit: BaseEmit):
-        """Adds the provided emit primitive to the EdnaContext
-
-        Args:
-            ingest (BaseEmit): Emit primitive for the EdnaContext
-        """
-        self.emit = emit
 
     def execute(self):
         """Execute the EdnaContext. This calls the `run()` method, which must be implemented in the inheriting class.
@@ -101,12 +79,6 @@ class EdnaContext(ABC):
             PrimitiveNotSetException: Raises this exception if `execute()` is called without setting all of the primitives.
         """
         # TODO process optimizations go here in future
-        if self.ingest is None:
-            raise PrimitiveNotSetException("StreamingContext", "Ingest")
-        if self.process is None:
-            raise PrimitiveNotSetException("StreamingContext", "Process")
-        if self.emit is None:
-            raise PrimitiveNotSetException("StreamingContext", "Emit")
         self.run()
 
-from .StreamingContext import StreamingContext
+from .SimpleStreamingContext import SimpleStreamingContext
