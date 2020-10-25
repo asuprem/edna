d990f06f52f8eab2ea6fac8324fd18074c960b35
Abhijit Suprem <asuprem@gatech.edu>
2020-10-25 17:42:25 -0400

    ✨feature: Added KeyedFilter process

---
+++ python/edna/src/edna/process/filter/KeyedFilter.py
@@ -0,0 +1,23 @@
+from typing import Callable
+from edna.process.filter import Filter
+from edna.process import BaseProcess
+
+
+class KeyedFilter(Filter):
+    """Removes values.
+
+    Args:
+        Filter (BaseProcess): The interface this process implements
+    """
+    # filter_callable  -->  boolean that takes in a value and returns true if it is to be removed
+    def __init__(self, 
+            key: str,
+            filter_callable: Callable,
+            process: BaseProcess = None, 
+            *args, **kwargs):
+        super().__init__(process=process, *args, **kwargs)
+        self.key = key
+        self.filter_callable = filter_callable
+    
+    def filter(self, message: str):
+        return [message] if self.filter_callable(message[self.key]) else []
