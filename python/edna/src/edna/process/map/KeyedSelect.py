ba12d824e5bde6f084ea822050ccbdc5fad920dd
Abhijit Suprem <asuprem@gatech.edu>
2020-11-16 21:17:17 -0500

    ✨feature: Added KeyedSelect map

---
+++ python/edna/src/edna/process/map/KeyedSelect.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+from typing import List
+from edna.process import BaseProcess
+from edna.process.map import Map
+
+class KeyedSelect(Map):
+    """Returns subset of dictionary
+
+    Args:
+        Map (BaseProcess): The interface this process implements
+    """
+    process_name : str = "KeyedSelect"
+
+    def __init__(self, 
+        keys = List[str],
+        process: BaseProcess = None, 
+        *args, **kwargs) -> BaseProcess:
+        self.keys = keys
+    def map(self, message: str):
+        return {key:message[key] for key in self.keys}
