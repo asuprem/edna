a59b23237943ae0e730b00cc06c0179124310ce4
Abhijit Suprem <asuprem@gatech.edu>
2021-02-04 18:52:27 -0500

    Added RecordCount to, well, countn records

---
+++ python/edna/src/edna/process/aggregate/RecordCount.py
@@ -0,0 +1,20 @@
+from edna.process.aggregate import Aggregate
+from edna.process import BaseProcess
+from edna.types.builtin import StreamRecord
+
+class RecordCount(Aggregate):
+    process_name: str = "RecordCount"    
+
+    def __init__(self, process: BaseProcess = None, *args, **kwargs) -> BaseProcess:
+        super().__init__(process=process, *args, **kwargs)
+        self.count = 0
+        self.triggeredEmit = [StreamRecord(0)]
+
+    def aggregate(self, record: object):
+        self.count += 1
+        #print("Current count ---- ", self.count)
+        self.triggeredEmit = [StreamRecord(self.count)]
+
+    def reset(self):
+        self.count = 0
+        self.triggeredEmit = [StreamRecord(0)]
